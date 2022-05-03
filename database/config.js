ef8b07ec34b99b733bd5d61ff5df3fdf3bc0f965
Erick Villafuerte <erick.m.villa.r@gmail.com>
2022-05-03 09:46:50 -0500

    corrigiendo errores heroku

--- database/config.js
+++ database/config.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose');
 const dbconection = async() => {
     try{
 
-        await mongoose.connect(process.env.MONGODB_CNN);
-        console.log('DB is connected');
+        mongoose.connect(process.env.MONGODB_CNN,{useNewUrlParser: true, useUnifiedTopology: true})
+            .then(() => console.log('DB is connected'))
 
     }catch(error){
         console.log(error);
-        throw new Error('Error to connect to database', error);
+        throw new Error('Error to connect to database');
     }
 }
-
 
 module.exports = {
     dbconection
